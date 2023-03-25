373ec7fce1c824044ee99bbae6b99ccdece5f91e
Chirag Sethiya <chiragsethiya123@gmail.com>
2023-03-25 19:27:12 +0530

    initial commit

---
+++ src/main/java/com/project/springbootkafkamicroservices/SpringBootKafkaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.springbootkafkamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springbootkafkamicroservices/SpringBootKafkaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springbootkafkamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
